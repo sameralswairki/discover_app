8e8eeeeaa4000d936ee45006b2b59eb1e92d4a4e
Samer.Alswairki <samer.alswairki@gmail.com>
2020-12-01 17:57:01 +0100

    make the code DRY and refactore it using partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-
-  def show
-    @article = Article.find(params[:id])
+  #Below code is using DRY (not to repeat things)
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+  def show  
   end
 
   def index
@@ -13,12 +14,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     #white listing only title and description
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to article_path(@article)
@@ -30,12 +30,28 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully. "
       redirect_to @article
     else
       render 'edit'
     end
   end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  #only available to this controller
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :create, :new, :edit, :update]
+  resources :articles
+  #or
+  #resources :articles, only: [:show, :index, :create, :new, :edit, :update, :destroy]
 end
