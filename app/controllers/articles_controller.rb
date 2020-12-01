6c492233814c2f4d146857831ada365a7e299d7b
Samer.Alswairki <samer.alswairki@gmail.com>
2020-12-01 08:47:29 +0100

    add validation and flash messages to the app within the layout page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,21 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #white listing only title and description
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to article_path(@article)
+      #as a shortcut:
+      #redirect_to @article
+    else
+      render 'new' 
+    end
+  end
+
 end
